287973c22a1086db7e179e1cec82554ce29b5173
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2021-11-25 20:10:06 +0100

    e

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import react from "./logofin/react.svg";
+import redux from "./logofin/redux.svg";
+import firebase from "./logofin/firebase.svg";
+import git from "./logofin/git.svg";
+import php from "./logofin/php.svg";
+import illustrator from "./logofin/illustrator.svg";
+import photoshop from "./logofin/photoshop.svg";
+import indesign from "./logofin/indesign.svg";
+import sass from "./logofin/sass.svg";
+import wordpress from "./logofin/wordpress.svg";
+import reactw from "./logofin/reactmewit.svg";
+import reduxw from "./logofin/reduxmewit.svg";
+import firebasew from "./logofin/firebasemewit.svg";
+import gitw from "./logofin/gitmewit.svg";
+import phpw from "./logofin/phpmewit.svg";
+import illustratorw from "./logofin/illustratormewit.svg";
+import photoshopw from "./logofin/photoshopmewit.svg";
+import indesignw from "./logofin/indesignmewit.svg";
+import sassw from "./logofin/sassmewit.svg";
+import wordpressw from "./logofin/wordpressmewit.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={react} className="App-logo" alt="logo" />
+        <img src={redux} className="App-logo" alt="logo" />{" "}
+        <img src={sass} className="App-logo" alt="logo" />
+        <img src={git} className="App-logo" alt="logo" />
+        <img src={firebase} className="App-logo" alt="logo" />
+        <img src={php} className="App-logo" alt="logo" />
+        <img src={wordpress} className="App-logo" alt="logo" />
+        <img src={illustrator} className="App-logo" alt="logo" />
+        <img src={photoshop} className="App-logo" alt="logo" />
+        <img src={indesign} className="App-logo" alt="logo" />
+        <img src={reactw} className="App-logo" alt="logo" />
+        <img src={reduxw} className="App-logo" alt="logo" />{" "}
+        <img src={sassw} className="App-logo" alt="logo" />
+        <img src={gitw} className="App-logo" alt="logo" />
+        <img src={firebasew} className="App-logo" alt="logo" />
+        <img src={phpw} className="App-logo" alt="logo" />
+        <img src={wordpressw} className="App-logo" alt="logo" />
+        <img src={illustratorw} className="App-logo" alt="logo" />
+        <img src={photoshopw} className="App-logo" alt="logo" />
+        <img src={indesignw} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
